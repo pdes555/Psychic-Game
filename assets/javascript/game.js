877f352c883199f2560f4993c0a3d3d0bf6cd629
pdes555 <philld5@yahoo.com>
2017-12-09 14:20:32 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,33 @@ let guessesLeft = 10
 let guesses = 0
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// document.getElementById("wins").innerHTML = wins;
-// make an array with all letters XXXXXXXXXXXXXXXXXXX
+document.getElementById("wins").innerHTML = wins++;
+document.getElementById("losses").innerHTML = losses++;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-// make a function that randomly selects a number from the array XXXXXXXXXXXXXXX
-
-//convert number to string XXXXXXXXXXX
-
-// make a function that catches what key im pressing XXXXXXXXXXXXXXXXXX
 
 let randomNumber = Math.floor(Math.random()*letters.length);
 let randomLetter = letters[randomNumber];
 
+
+	
 document.onkeydown = function(event) {
 	let keypress = String.fromCharCode(event.keyCode);
 	console.log(keypress);
 	
 	if (keypress === randomLetter) {
-		wins++
+		document.getElementById("wins").innerHTML = wins++;
+		randomNumber = Math.floor(Math.random()*letters.length);
 	} else {
-		guessesLeft--
+		document.getElementById("guessesLeft").innerHTML = guessesLeft--;
 		document.getElementById("guesses").append(keypress + ", ");
 	}
+	if (guessesLeft === -1) {
+	guessesLeft = 10;
+	document.getElementById("losses").innerHTML = losses++;
+}
 }
 
-if (guessesLeft === 0) {
-	losses++
-	document.getElementById(guessesLeft).append = ("");
-}
-
-// make an if statement that checks if that key matches the letter in the array XXXXXXXXXXXXX
-
-// if yes add to wins and run the function to select a new letter from the array
-
-// if not add that letter to guesses take away 1 from guessesLeft and show that letter in guesses
-
-// make an if statement that says if guessesLeft makes it to 0 then add 1 to losses and run funtion that picks a new random letter
 
 
 
